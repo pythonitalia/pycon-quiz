26982f72dec3218a60512f0a1507c0e959a7f480
Marco Acierno <marcoaciernoemail@gmail.com>
2020-04-18 17:52:29 +0000

    Nextjs frontend

--- backend/api/game_manager/subscription.py
+++ backend/api/game_manager/subscription.py
@@ -5,6 +5,7 @@ from game_manager.session import get_redis_channel_name_for_session_id
 from api.game_manager.types import GameState
 
 from pycon_quiz.redis import get_client
+from game_manager.session import get_session
 
 
 @strawberry.type
@@ -14,12 +15,19 @@ class GameManagerSubscription:
         self, info, session_id: int
     ) -> typing.AsyncGenerator[GameState, None]:
         client = await get_client()
-        connection = await client.subscribe(
-            get_redis_channel_name_for_session_id(session_id)
-        )
-        channel = connection[0]
 
         try:
+            connection = await client.subscribe(
+                get_redis_channel_name_for_session_id(session_id)
+            )
+            channel = connection[0]
+
+            # get the state from the DB the first time
+            # TODO: Update to store in redis the last published session?
+            # possible race condition?
+            session = await get_session(session_id)
+            yield GameState.from_session(session)
+
             while await channel.wait_message():
                 message = await channel.get_json()
                 yield GameState.from_data(message)
---
+++ frontend/src/pages/play/[session].tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Session = () => <div>hey</div>;
+
+export default Session;
