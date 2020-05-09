4ec660054dced276c31d1c17864410c45739094b
Marco Acierno <marcoaciernoemail@gmail.com>
2020-05-09 11:17:59 +0000

    Fix eslint warning about keys and array index

--- frontend/src/components/background/index.tsx
+++ frontend/src/components/background/index.tsx
@@ -13,6 +13,7 @@ export const Background: React.SFC<Props> = (props) => {
       {Array(howManyQuestionMarks)
         .fill(null, 0)
         .map((_, i) => (
+          // eslint-disable-next-line react/no-array-index-key
           <QuestionMark key={i} />
         ))}
     </Box>
