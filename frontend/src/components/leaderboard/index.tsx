c240a8d43a707579a564d9a1a3d39dda9bc22e68
Marco Acierno <marcoaciernoemail@gmail.com>
2020-05-13 23:01:35 +0000

    Cleanup Leaderboard

--- frontend/src/components/leaderboard/index.tsx
+++ frontend/src/components/leaderboard/index.tsx
@@ -8,45 +8,43 @@ type Props = {
   leaderboard: LeaderboardPartecipant[];
 };
 
-export const Leaderboard: React.SFC<Props> = ({ leaderboard }) => {
-  return (
-    <Box
+export const Leaderboard: React.SFC<Props> = ({ leaderboard }) => (
+  <Box
+    sx={{
+      maxWidth: "52rem",
+      mx: "auto",
+      px: "primary",
+      flexDirection: "column",
+      height: "auto",
+      minHeight: "100vh",
+    }}
+    variant="layouts.center"
+  >
+    <Heading
       sx={{
-        maxWidth: "52rem",
-        mx: "auto",
-        px: "primary",
-        flexDirection: "column",
-        height: "auto",
-        minHeight: "100vh",
+        letterSpacing: "0.3em",
+        textAlign: "center",
+        mt: "10rem",
+        mb: "6rem",
       }}
-      variant="layouts.center"
     >
-      <Heading
-        sx={{
-          letterSpacing: "0.3em",
-          textAlign: "center",
-          mt: "10rem",
-          mb: "6rem",
-        }}
-      >
-        Leaderboard
-      </Heading>
+      Leaderboard
+    </Heading>
 
-      <Grid
-        sx={{
-          gridTemplateColumn: "1fr",
-          gridGap: "3rem",
-          justifyItems: ["center", "flex-end"],
-        }}
-      >
-        {leaderboard.map((partecipant, position) => (
-          <PartecipantScore
-            key={partecipant.name}
-            partecipant={partecipant}
-            position={position}
-          />
-        ))}
-      </Grid>
-    </Box>
-  );
-};
+    <Grid
+      sx={{
+        gridTemplateColumn: "1fr",
+        gridGap: "3rem",
+        justifyItems: ["center", "flex-end"],
+      }}
+    >
+      {leaderboard.map((partecipant, position) => (
+        <PartecipantScore
+          key={partecipant.name}
+          partecipant={partecipant}
+          position={position}
+        />
+      ))}
+    </Grid>
+  </Box>
+);
