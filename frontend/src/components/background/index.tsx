e7be47634b1fc508249b2e0f814a7ec403c947b6
Marco Acierno <marcoaciernoemail@gmail.com>
2020-05-22 14:07:28 +0000

    Fix background zIndex level

--- frontend/src/components/background/index.tsx
+++ frontend/src/components/background/index.tsx
@@ -16,6 +16,7 @@ export const Background: React.SFC<Props> = (props) => (
       width: "100%",
       height: "100%",
       overflow: "hidden",
+      zIndex: -1,
     }}
   >
     {Array(howManyQuestionMarks)
