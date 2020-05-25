f9e7792523ab056a8dbb2e58588804308f7d1104
Marco Acierno <marcoaciernoemail@gmail.com>
2020-05-25 23:03:21 +0000

    Fix tsc

--- frontend/src/components/partecipant-score/index.tsx
+++ frontend/src/components/partecipant-score/index.tsx
@@ -8,67 +8,69 @@ type Props = {
   partecipant: LeaderboardPartecipant;
   position: number;
   showCurrentPlayerMarker: boolean;
+  ref: React.Ref<HTMLDivElement>;
 };
 
-export const PartecipantScore: React.ForwardRefExoticComponent<Props> = React.forwardRef(
-  ({ partecipant, position, showCurrentPlayerMarker }, ref) => (
-    <Flex
-      ref={ref}
+export const PartecipantScore: React.ForwardRefExoticComponent<Props> = React.forwardRef<
+  HTMLDivElement,
+  Props
+>(({ partecipant, position, showCurrentPlayerMarker }, ref) => (
+  <Flex
+    ref={ref}
+    sx={{
+      width: "100%",
+      position: "relative",
+      justifyContent: "flex-end",
+    }}
+  >
+    <Text
+      variant="layouts.center"
+      as="span"
       sx={{
-        width: "100%",
-        position: "relative",
-        justifyContent: "flex-end",
+        fontSize: "header",
+        mr: "primary",
+        width: ["1.5rem", "3.5rem"],
+      }}
+      role="img"
+      aria-label="Your name marker"
+    >
+      {showCurrentPlayerMarker && "👉"}
+    </Text>
+    <Flex
+      sx={{
+        border: "primary",
+        width: [
+          position === 0 ? "100%" : "calc(100% - 5rem)",
+          position === 0 ? "100%" : "calc(100% - 10rem)",
+        ],
       }}
     >
-      <Text
-        variant="layouts.center"
-        as="span"
+      <Box
         sx={{
-          fontSize: "header",
-          mr: "primary",
-          width: ["1.5rem", "3.5rem"],
-        }}
-        role="img"
-        aria-label="Your name marker"
-      >
-        {showCurrentPlayerMarker && "👉"}
-      </Text>
-      <Flex
-        sx={{
-          border: "primary",
-          width: [
-            position === 0 ? "100%" : "calc(100% - 5rem)",
-            position === 0 ? "100%" : "calc(100% - 10rem)",
-          ],
+          flexGrow: 1,
+          px: "primaryHorizontal",
+          py: "primaryVertical",
+          textTransform: "uppercase",
+          backgroundColor: partecipant.color,
+          display: "flex",
+          alignItems: "center",
+          wordBreak: "break-all",
         }}
       >
-        <Box
-          sx={{
-            flexGrow: 1,
-            px: "primaryHorizontal",
-            py: "primaryVertical",
-            textTransform: "uppercase",
-            backgroundColor: partecipant.color,
-            display: "flex",
-            alignItems: "center",
-            wordBreak: "break-all",
-          }}
-        >
-          {partecipant.name}
-        </Box>
-        <Box
-          sx={{
-            flexShrink: 0,
-            py: "1rem",
-            px: "3.6rem",
-            borderLeft: "primary",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          {partecipant.score}
-        </Box>
-      </Flex>
+        {partecipant.name}
+      </Box>
+      <Box
+        sx={{
+          flexShrink: 0,
+          py: "1rem",
+          px: "3.6rem",
+          borderLeft: "primary",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        {partecipant.score}
+      </Box>
     </Flex>
-  )
-);
+  </Flex>
+));
