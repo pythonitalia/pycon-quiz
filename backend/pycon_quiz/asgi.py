d1d138e99160af3357f29c1bc854887e8e66fa48
Marco Acierno <marcoaciernoemail@gmail.com>
2020-06-06 19:39:25 +0000

    Send KeepAlive from the backend every 30 seconds

--- backend/pycon_quiz/asgi.py
+++ backend/pycon_quiz/asgi.py
@@ -12,9 +12,9 @@ import os
 from django.core.asgi import get_asgi_application
 from starlette.applications import Starlette
 from starlette.websockets import WebSocketDisconnect
-from strawberry.asgi import GraphQL
 
 from api.schema import schema
+from strawberry.asgi import GraphQL
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pycon_quiz.settings.dev")
 
@@ -26,7 +26,9 @@ async def application(scope, receive, send):
         await django_application(scope, receive, send)
     elif scope["type"] == "websocket":
         try:
-            graphql_app = GraphQL(schema, debug=True)
+            graphql_app = GraphQL(
+                schema, debug=True, keep_alive=True, keep_alive_interval=30
+            )
             app = Starlette(debug=True)
             app.add_websocket_route("/graphql", graphql_app)
             await app(scope, receive, send)
