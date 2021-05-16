410517bdb16423c3cd13e4d9701a61af1d103aeb
Marco Acierno <marcoaciernoemail@gmail.com>
2021-05-17 01:47:51 +0200

    Change s3 domain

--- backend/pycon_quiz/settings/prod.py
+++ backend/pycon_quiz/settings/prod.py
@@ -5,5 +5,5 @@ SECRET_KEY = env("SECRET_KEY")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN = f"d2x84vjkgthxda.cloudfront.net"
+AWS_S3_CUSTOM_DOMAIN = "d4v30sgvhcss3.cloudfront.net"
 DEFAULT_FILE_STORAGE = "pycon_quiz.storages.MediaStorage"
