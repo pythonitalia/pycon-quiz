87c96afddec0e21a3cc6b94af4ac37b7a796eae3
Etty <beltrami.ester@gmail.com>
2021-06-15 22:12:16 +0100

    Add export scrpt

---
+++ backend/export.py
@@ -0,0 +1,23 @@
+from quizzes.models import *
+import csv
+
+FILE_NAME = "export.csv"
+COLUMNS = ("Question", "Answer 1", "Answer 2", "Answer 3", "Answer 4", "Correct Answer")
+questions = Question.objects.all()
+
+
+rows = []
+for q in questions:
+    row = [q.text]
+    for a in q.answers.all():
+        row.append(a.text)
+    rows.append(row)
+
+
+with open(FILE_NAME, "w", newline="") as csvfile:
+    spamwriter = csv.writer(
+        csvfile
+    )
+    spamwriter.writerow(COLUMNS)
+    for row in rows:
+        spamwriter.writerow(row)
